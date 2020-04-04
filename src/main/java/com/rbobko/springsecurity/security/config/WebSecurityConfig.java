1af6d95ad930581bfe193cca6b6aad5367ba6edc
Rafal Bobko <rbobko70@gmail.com>
2020-04-04 17:32:57 -0400

    feat(srv): Adding security and allowing anonymous access to h2 console.

---
+++ src/main/java/com/rbobko/springsecurity/security/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.rbobko.springsecurity.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable();
+    }
+}
